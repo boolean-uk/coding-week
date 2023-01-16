191a10793048eed33f3cacd7e0e6d8a9744b7973
dearshrewdwit <dearshrewdwit@gmail.com>
2023-01-16 16:04:53 +0000

    update lang

--- 01-html-css-fundamentals/18-for-loop/script.js
+++ 01-html-css-fundamentals/18-for-loop/script.js
@@ -3,7 +3,7 @@ for (let i = 0; i < 10; i++) {
     console.log("Count:" + i);
 }
 
-// Iterazione array
+// Iteration array
 const names = ['Edward', 'Nathan', 'Jules'];
 for (let i = 0; i < names.length; i++) {
     console.log(names[i]);
